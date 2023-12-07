e735e3e4a9b324c3b5612c3adef289821a7f6b1c
renatex314 <renatocorte34@gmail.com>
2023-12-07 16:43:04 -0300

    commit inicial

---
+++ arduino.cpp
@@ -0,0 +1,44 @@
+#define GREEN_PIN 2
+#define YELLOW_PIN 4
+#define RED_PIN 7
+
+// definição das funções
+void allowedSignal()
+{
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(YELLOW_PIN, LOW);
+  digitalWrite(GREEN_PIN, HIGH);
+}
+
+void warningSignal()
+{
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(YELLOW_PIN, HIGH);
+  digitalWrite(GREEN_PIN, LOW);
+}
+
+void forbiddenSignal()
+{
+  digitalWrite(RED_PIN, HIGH);
+  digitalWrite(YELLOW_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
+}
+// definição das funções
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  allowedSignal();
+  delay(10000);
+  warningSignal();
+  delay(3000);
+  forbiddenSignal();
+  delay(10000);
+}
